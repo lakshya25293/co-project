0c04f03ec14fedae470e7d4b1a9c23bf6470a400
Deepta <deepta25170@iiitd.ac.in>
2026-03-04 17:51:22 +0000

    Added function to convert instructions to binary

--- i-type.py
+++ i-type.py
@@ -12,7 +12,7 @@ def load():
             for i in L:
                 j = i.replace(",","").split()
                 S.append(j)'''
-        L = "addi x2, x3, 10"
+        L = "lw x2, 5(x4)"
         j = L.replace(",","").split()
         S.append(j)
         print(S)    
@@ -21,9 +21,32 @@ load()
 #Function to convert immediate value to binary
 def imm_to_bin(imm):
     return format(int(imm),"012b")
-            
+
+#Function to convert register value to binary            
+def reg_to_bin(reg):
+    return format(int(reg), "05b")            
      
+#Function to convert instructions to binary     
+def toBinary(S):
+    for x in S:
+        b = instr.get(x[0])
+        funct3, opcode = b
+
+        if x[0] == "lw":
+            rd = reg_to_bin(x[1][1:])   
+            imm, rs1 = x[2].replace(")", "").split("(") #Removing brackets 
+            rs = reg_to_bin(rs1[1:])    
+            imm = imm_to_bin(imm)
+        else:
+            rd = reg_to_bin(x[1][1:])
+            rs = reg_to_bin(x[2][1:])
+            imm = imm_to_bin(x[3])
+
+        print(imm + rs + funct3 + rd + opcode)
+toBinary(S)          
+
+                      
+                        
 
               
-
 
